7929c81624e12c21a50560c34e40727d7eeb86a2
Your Name <you@example.com>
2021-06-25 20:33:38 +0300

    images, conditional class and counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 1,
+    imgUrl: 'https://placeimg.com/340/280/tech',
+  };
+
+  h2ElStyles = {
+    background: 'tomato',
+    color: 'snow',
+    textAlign: 'center',
+  };
   render() {
     return (
       <div className="container mt-4">
-        <h2>I am react counter component</h2>
-        <span className="badge badge-info mr-3">10</span>
+        <h2 style={{ fontSize: '2em' }}>I am react counter component</h2>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-warning">Press me</button>
+        <div className="mt-5">
+          <img src={this.state.imgUrl} alt="tech" />
+        </div>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let badgeClasses = 'badge mr-3 badge-';
+    badgeClasses += this.state.count === 0 ? 'danger' : 'info';
+    return badgeClasses;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Out of stock' : count;
   }
 }
 
